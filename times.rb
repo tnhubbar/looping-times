6ce9745a70a99310d626cab7dfef25f55a027d18
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-11 06:31:20 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,6 +1,10 @@
+# Fill out the content of the `using_times` method so that calling it will `puts` the desired phrase, `"Wingardium Leviosa"` seven times by using the `times` keyword.
+
 def using_times
-	#your code here
+	7.times do 
+	  puts "Wingardium Leviosa"
 end
+end 
 
 
 
