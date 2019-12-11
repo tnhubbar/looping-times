7a930c6c181e2695e6c62b44cb8c5c79fd9ace62
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-11 06:30:39 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,3 +1,5 @@
+# Fill out the content of the `using_times` method so that calling it will `puts` the desired phrase, `"Wingardium Leviosa"` seven times by using the `times` keyword.
+
 def using_times
 	#your code here
 end
